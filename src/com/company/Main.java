c1e680cc0f60e683038e841e9812da77a21669f8
893319427 <893319427@qq.com>
2019-08-08 14:56:34 +0800

    ddx

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,14 @@
 package com.company;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        Map map = new HashMap();
+
     }
 }
